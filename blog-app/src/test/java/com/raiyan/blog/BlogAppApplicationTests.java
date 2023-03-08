b1541ab909bc14e768b6e3cd1b9cf476b2c2ae78
raiyan22 <raiyanislam058@gmail.com>
2023-03-08 08:36:48 -0800

    initial run

---
+++ blog-app/src/test/java/com/raiyan/blog/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raiyan.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
